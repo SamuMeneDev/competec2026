6bffd1656cc554dffbd9e00e01d10273bb8c750b
Samu Mene <152662299+SamuMeneDev@users.noreply.github.com>
2026-05-13 21:39:19 -0300

    Add Exercicio0.java with variable examples

---
+++ aula-0/Exercicio0.java
@@ -0,0 +1,29 @@
+public class Exercicio0
+{
+	public static void main(String[] args) {
+		/* Variáveis*/
+		
+		// Ex 1
+		char primeiraLetra = 'S';
+		
+		// Ex 2
+		short idade = 17;
+		
+		// Ex 3
+		
+		boolean tomeiCafe = true;
+	
+	    /* Variáveis II */
+	    
+	    // Ex 1
+	    float precoLaranja = 5.6f;
+	    
+	    // Ex 2
+	    float precoUva = 12.3f;
+	    
+	    // Ex 3
+	    float somaFrutas = precoLaranja + precoUva;
+	    System.out.println("Laranja + Uva = "+somaFrutas);
+	    
+	}
+}
